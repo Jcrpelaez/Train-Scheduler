f78a8badf0a11c24e6552a58b8fe993da61028d1
Juan Carlos <jcrpelaez@yahoo.com>
2019-11-08 11:36:37 -0600

    created js folder

---
+++ javascipt.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCgVs7nt2jvnJDymLCc03teYuE3-AvN8xI",
+  authDomain: "test-project-utsa.firebaseapp.com",
+  databaseURL: "https://test-project-utsa.firebaseio.com",
+  projectId: "test-project-utsa",
+  storageBucket: "test-project-utsa.appspot.com",
+  messagingSenderId: "392920990156",
+  appId: "1:392920990156:web:d9e0705f24fb1032db1d69",
+  measurementId: "G-HMYCEMW4KE"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
